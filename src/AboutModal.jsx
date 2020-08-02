86eb5e0047078452ff00015d2cae20c9ac155cf1
Sunny Ng <sng@squarespace.com>
2020-08-01 20:33:28 -0400

    Update AboutModal copy

--- src/AboutModal.jsx
+++ src/AboutModal.jsx
@@ -13,8 +13,8 @@ class AboutModal extends React.Component {
         <Modal.Content>
           <Modal.Description>
             <p>
-              Shortly after 50-a—the statue in New York state shielding police disciplinary records from the public, was repealed, ProPublica
-              <a href='https://www.propublica.org/article/nypd-civilian-complaint-review-board-editors-note' target='_blank' rel="noopener noreferrer">published over 12,000
+              Shortly after 50-a—the statue in New York state shielding police disciplinary records from the public, was repealed,
+              ProPublica <a href='https://www.propublica.org/article/nypd-civilian-complaint-review-board-editors-note' target='_blank' rel="noopener noreferrer">published over 12,000
               records</a> of civilian complaints filed against New York City police officers on July 26, 2020.
             </p>
             <p>
@@ -29,16 +29,16 @@ class AboutModal extends React.Component {
             <p>
               Uses <a href='https://www.propublica.org/datastore/dataset/civilian-complaints-against-new-york-city-police-officers' target='_blank' rel="noopener noreferrer">
               Civilian Complaints Against New York City Police Officers              
-              </a> dataset provided by ProPublica, published in July 2020.
+              </a> dataset published by ProPublica in July 2020.
             </p>
             <p>
-              Precinct map data provided by <a href='https://data.cityofnewyork.us/Public-Safety/Police-Precincts/78dh-3ptz' target='_blank' rel="noopener noreferrer">NYC Open Data</a>.
+              Precinct map data from <a href='https://data.cityofnewyork.us/Public-Safety/Police-Precincts/78dh-3ptz' target='_blank' rel="noopener noreferrer">NYC Open Data</a>.
             </p>
             <p>
               Created by <a href='https://sunny.ng' target='_blank' rel="noopener noreferrer">Sunny Ng</a>.
             </p>
             <p>
-              Source code on <a href='https://github.com/blahblahblah-/nypd-complaints' target='_blank' rel="noopener noreferrer">Github</a>.
+              Source code on <a href='https://github.com/blahblahblah-/nypd-complaints' target='_blank' rel="noopener noreferrer">GitHub</a>.
             </p>
           </Modal.Description>
         </Modal.Content>
